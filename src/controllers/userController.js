77f0df053f18bc7ab2639a2684a5db391a829c9a
kangkyeongseo <kks_big@naver.com>
2022-08-23 17:25:48 +0900

    User Recipes Page

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -57,5 +57,12 @@ export const getUserIng = (req, res) => {
 };
 
 export const getUserRecipe = (req, res) => {
-  return res.render("user/user-edit");
+  const recipes = [
+    {
+      title: "오징어 볶음",
+      description: "쫄깃쫄깃 오징어와 매콤 달달 양념의 조화! 밥 두공기 뚝딱!",
+      time: 20,
+    },
+  ];
+  return res.render("user/user-recipes", { recipes });
 };
