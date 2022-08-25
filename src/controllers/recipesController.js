eddc5eabc4e3472c132935f0b40c837868dd58ca
kangkyeongseo <kks_big@naver.com>
2022-08-25 15:08:53 +0900

    Recipe Edit Page

--- src/controllers/recipesController.js
+++ src/controllers/recipesController.js
@@ -40,4 +40,36 @@ export const getRecipesDetail = (req, res) => {
   return res.render("recipe/recipe-detail", { recipe });
 };
 
-export const getRecipesEdit = (req, res) => res.send("recipes edit");
+export const getRecipesEdit = (req, res) => {
+  const recipe = {
+    name: "오징어볶음",
+    serving: "1",
+    time: "20",
+    ingredients: [
+      { id: 1, name: "오징어", amount: "300", amountType: "g" },
+      {
+        id: 2,
+        name: "양파",
+        amount: "1",
+        amountType: "개",
+      },
+      {
+        id: 3,
+        name: "고추장",
+        amount: "1",
+        amountType: "Ts",
+      },
+    ],
+    orders: [
+      {
+        order: 1,
+        text: "손질을 합니다",
+      },
+      {
+        order: 2,
+        text: "휘리릭 찹찹",
+      },
+    ],
+  };
+  return res.render("recipe/recipe-edit", { recipe });
+};
