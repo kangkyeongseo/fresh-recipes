cdb2f4e1acf75f299b32b1a097e250613f9c2b8b
kangkyeongseo <kks_big@naver.com>
2022-08-25 17:39:41 +0900

    Recipe Model

--- src/db.js
+++ src/db.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import User from "./model/User";
+import Ingredient from "./model/Ingredient";
 
 mongoose.connect("mongodb://127.0.0.1:27017/fresh-recipes");
 
---
+++ src/model/Recipe.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const recipeSchema = new mongoose.Schema({
+  name: { type: String, require: true, trim: true },
+  serving: { type: Number, require: true },
+  time: { type: Number, require: true },
+  imgredients: [
+    {
+      ingredient: { type: String, require: true },
+      ingredientAmount: { type: String, require: true },
+      amountType: { type: Number, require: true },
+    },
+  ],
+  orders: [
+    {
+      order: { type: Numvber, require: true },
+      content: { type: String, require: true },
+    },
+  ],
+});
+
+const Recipe = mongoose.model("Recipe", recipeSchema);
+
+export default Recipe;
