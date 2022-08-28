c783e86beb955b8c73838b1f3487fbeb39acd079
kangkyeongseo <kks_big@naver.com>
2022-08-28 20:27:43 +0900

    User Detail

--- middleware.js
+++ middleware.js
@@ -2,6 +2,5 @@ export const sessionMiddleware = (req, res, next) => {
   res.locals.loggedIn = Boolean(req.session.loggedIn);
   res.locals.loggedInUser = req.session.user || {};
   res.locals.homeTitle = "Fresh Recipes";
-  console.log(res.locals);
   next();
 };
--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,38 +1,10 @@
-export const getUserDetail = (req, res) => {
+import User from "../model/User";
+
+export const getUserDetail = async (req, res) => {
   const {
     params: { id },
   } = req;
-  const user = {
-    id,
-    name: "kks",
-    ingredients: [
-      {
-        name: "감자",
-        amount: "3개",
-        freshPeriod: 1,
-      },
-      {
-        name: "당근",
-        amount: "1개",
-        freshPeriod: 2,
-      },
-    ],
-    recipes: [
-      {
-        title: "오징어 볶음",
-        description: "쫄깃쫄깃 오징어와 매콤 달달 양념의 조화! 밥 두공기 뚝딱!",
-        time: 20,
-      },
-    ],
-    likeRecipes: [
-      {
-        title: "연어장 덮밥",
-        description: "부드럽고 고소한 연어와 짭쪼름한 간장의 조화!",
-        time: 90,
-      },
-    ],
-  };
-
+  const user = await User.findById(id);
   return res.render("user/user-detail", { user });
 };
 
