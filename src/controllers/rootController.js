d17b31bbf2838cb467e2e453d2f6f9ac6fe3e9c6
kangkyeongseo <kks_big@naver.com>
2022-09-17 22:38:02 +0900

    Random Recipe Print on Home Page

--- src/controllers/rootController.js
+++ src/controllers/rootController.js
@@ -1,12 +1,10 @@
 import User from "../model/User";
 import bcrypt from "bcrypt";
+import Recipe from "../model/Recipe";
 
 export const getHome = async (req, res) => {
-  const recipe = {
-    title: "오징어 볶음",
-    description: "쫄깃쫄깃 오징어와 매콤 달달 양념의 조화! 밥 두공기 뚝딱!",
-    time: 20,
-  };
+  const recipe = await Recipe.aggregate([{ $sample: { size: 1 } }]);
+  console.log(recipe);
   if (req.session.loggedIn) {
     const {
       session: {
