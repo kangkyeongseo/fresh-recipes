23fccec9af8191ffc600c51afdcffc6d3bbd1c57
kangkyeongseo <kks_big@naver.com>
2022-09-17 22:18:34 +0900

    Square Page

--- src/controllers/squareController.js
+++ src/controllers/squareController.js
@@ -1,11 +1,7 @@
-export const getSquare = (req, res) => {
-  const recipes = [
-    {
-      title: "오징어 볶음",
-      description: "쫄깃쫄깃 오징어와 매콤 달달 양념의 조화! 밥 두공기 뚝딱!",
-      time: 20,
-    },
-  ];
+import Recipe from "../model/Recipe";
+
+export const getSquare = async (req, res) => {
+  const recipes = await Recipe.find({});
   return res.render("square/square-recipes", { recipes });
 };
 
