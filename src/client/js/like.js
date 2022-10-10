5869dca4555fa96b575cce0637a9f00124352c92
kangkyeongseo <kks_big@naver.com>
2022-10-10 19:20:37 +0900

    Count Likes

--- src/client/js/like.js
+++ src/client/js/like.js
@@ -1,24 +1,29 @@
 const recipe = document.querySelector(".recipe");
 const emptyHeart = document.querySelector(".far.fa-heart");
 const fillHeart = document.querySelector(".fas.fa-heart");
+const likesCount = document.querySelector(".likes__count");
 
 const { id } = recipe.dataset;
 
 const handleEmptyHeart = async () => {
-  emptyHeart.classList.add("hidden");
-  fillHeart.classList.remove("hidden");
-  await fetch(`/api/recipe/${id}/like/add`, {
-    method: "POST",
-  });
-};
-
-const handleFillHeart = async () => {
   fillHeart.classList.add("hidden");
   emptyHeart.classList.remove("hidden");
   await fetch(`/api/recipe/${id}/like/delete`, {
     method: "POST",
   });
+  const currentCount = likesCount.innerText;
+  likesCount.innerText = +currentCount - 1;
 };
 
-emptyHeart.addEventListener("click", handleEmptyHeart);
-fillHeart.addEventListener("click", handleFillHeart);
+const handleFillHeart = async () => {
+  emptyHeart.classList.add("hidden");
+  fillHeart.classList.remove("hidden");
+  await fetch(`/api/recipe/${id}/like/add`, {
+    method: "POST",
+  });
+  const currentCount = likesCount.innerText;
+  likesCount.innerText = +currentCount + 1;
+};
+
+emptyHeart.addEventListener("click", handleFillHeart);
+fillHeart.addEventListener("click", handleEmptyHeart);
