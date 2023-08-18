7b3ccf318e6638846e721f6f9b25178b9a05f8ac
kangkyeongseo <kks_big@naver.com>
2023-08-18 18:31:00 +0900

    style: server.js 주석 추가

--- src/server.js
+++ src/server.js
@@ -1,26 +1,27 @@
 import "dotenv/config";
 import "./db";
 import express from "express";
+import session from "express-session";
+import flash from "express-flash";
+import MongoStore from "connect-mongo";
+import morgan from "morgan";
+import { sessionMiddleware } from "../middleware";
 import ingredientsRouter from "./router/IngredientsRouter";
 import recipesRouter from "./router/recipesRouter";
 import rootRouter from "./router/rootRouter";
 import squareRouter from "./router/squareRouter";
 import userRouter from "./router/userRouter";
-import session from "express-session";
-import MongoStore from "connect-mongo";
-import morgan from "morgan";
-import flash from "express-flash";
-import { sessionMiddleware } from "../middleware";
 import apiRouter from "./router/apiRouter";
 
+// Express application을 생성합니다.
 const app = express();
 const PORT = 3000;
-
+// HTTP request logger middleware인 morgan을 생성합니다.
 const logger = morgan("dev");
-
+// View Engine으로 Pug를 사용합니다.
 app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
-
+// express-session을 적용합니다.
 app.use(
   session({
     secret: "secret",
@@ -31,15 +32,19 @@ app.use(
     }),
   })
 );
+// Body Parser
 app.use(express.urlencoded({ extened: true }));
 app.use(express.json());
+// 정적 파일을 사용합니다.
 app.use("/uploads", express.static("uploads"));
 app.use("/assets", express.static("assets"));
+// morgan을 사용합니다.
 app.use(logger);
+// express-flash를 사용합니다.
 app.use(flash());
-
+// Response Object의 Locals에 세션정보를 저장히기 위한 middleware입니다.
 app.use(sessionMiddleware);
-
+// express.Router 클래스를 사용하여 모듈식 마운팅 가능한 핸들러를 구성합니다.
 app.use("/", rootRouter);
 app.use("/user", userRouter);
 app.use("/ingredient", ingredientsRouter);
@@ -48,5 +53,5 @@ app.use("/square", squareRouter);
 app.use("/api", apiRouter);
 
 const handleListen = () => console.log(`Listening on http://localhost${PORT}`);
-
+//지정된 포트에 연결합니다.
 app.listen(PORT, handleListen);
