92c45059143e90cfc9a49fd50b5eb1457ac9c82e
kangkyeongseo <kks_big@naver.com>
2023-08-21 22:50:02 +0900

    style: userRouter.js 주석 추가

--- src/router/userRouter.js
+++ src/router/userRouter.js
@@ -14,29 +14,30 @@ import {
   postUserPasswordEdit,
 } from "../controllers/userController";
 
+// User Router를 생성합니다.
 const userRouter = express.Router();
 
-// User Detail Router
+// User Detail Router ("/user/:id")
 userRouter.get("/:id", getUserDetail);
-// User Edit Router
+// User Edit Router ("/user/:id/edit")
 userRouter
   .route("/:id/edit")
   .all(UserOnlyMiddleware)
   .get(getUserEdit)
   .post(avatarUpload.single("avatar"), postUserEdit);
-// Password Edit Router
+// User Password Edit Router ("/user/:id/password")
 userRouter
   .route("/:id/password")
   .all(UserOnlyMiddleware)
   .get(getUserPasswordEdit)
   .post(postUserPasswordEdit);
-// User Ingredients Router
+// User Ingredients Router ("/user/:id/ingredinets")
 userRouter.get("/:id/ingredients", getUserIng);
-// User Recipes Router
+// User Recipes Router ("/user/:id/recipes")
 userRouter.route("/:id/recipes").get(getUserRecipe);
-// User Likes Recipes
+// User Likes Recipes ("/user/:id/likes")
 userRouter.get("/:id/likes", getUserLike);
-
+// Github Login Router
 userRouter.get("/github/login", githubLogin);
 userRouter.get("/github/finish", githubFinish);
 
