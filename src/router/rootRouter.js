d8a59f127b09871c932906c730649d48f6ab5cf9
kangkyeongseo <kks_big@naver.com>
2023-08-21 12:56:32 +0900

    style: rootRouter.js 주석 추가

--- src/router/rootRouter.js
+++ src/router/rootRouter.js
@@ -9,19 +9,21 @@ import {
   postLogin,
 } from "../controllers/rootController";
 
+// Root Router를 생성합니다.
 const rootRouter = express.Router();
-
-// Home Router
+// Home Router("/")
 rootRouter.get("/", getHome);
-// Login Router
+// Login Router("/login")
 rootRouter
   .route("/login")
+  // PublicOnlyMiddleware은 세션에 로그인 정보가 없는 사용자만을 접근시켜주는 middleware입니다.
   .all(PublicOnlyMiddleware)
   .get(getLogin)
   .post(postLogin);
-// Logout Router
+// Logout Router("/logout")
+// UserOnlyMiddleware은 세션에 로그인 정보가 있는 사용자만을 접근시켜주는 middleware입니다.
 rootRouter.get("/logout", UserOnlyMiddleware, getLogout);
-// Join Router
+// Join Router("/join")
 rootRouter.route("/join").all(PublicOnlyMiddleware).get(getJoin).post(postJoin);
 
 export default rootRouter;
