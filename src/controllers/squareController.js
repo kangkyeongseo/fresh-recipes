f817be553ee673218728c41106b9a4df0ff1f2b8
kangkyeongseo <kks_big@naver.com>
2023-08-21 22:23:33 +0900

    style: squareController.js 주석 추가

--- src/controllers/squareController.js
+++ src/controllers/squareController.js
@@ -1,14 +1,17 @@
 import Recipe from "../model/Recipe";
 
+// getSquare Controller
 export const getSquare = async (req, res) => {
+  // Recipe 데이터를 불러옵니다.
   const recipes = await Recipe.find({});
   return res.render("square/square-recipes", { recipes });
 };
-
+// searchSquare Controller
 export const searchSquare = async (req, res) => {
   const {
     query: { keyword },
   } = req;
+  // query의 keyword값을 이용하여 Recipe 데이터를 불러옵니다.
   try {
     const recipes = await Recipe.find({ name: keyword });
     return res.render("square/square-recipes", { recipes });
