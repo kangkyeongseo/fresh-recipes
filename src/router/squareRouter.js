df5bd63845845b9788e7ab0c7f72626fdb9fddb0
kangkyeongseo <kks_big@naver.com>
2023-08-21 22:23:21 +0900

    style: squareRouter.js 주석 추가

--- src/router/squareRouter.js
+++ src/router/squareRouter.js
@@ -1,9 +1,11 @@
 import express from "express";
 import { getSquare, searchSquare } from "../controllers/squareController";
 
+// Sqaure Router를 생성합니다.
 const squareRouter = express.Router();
-
+// Sqaure Router("/square")
 squareRouter.route("/").get(getSquare);
+// Sqaure Search Router("/square/search")
 squareRouter.route("/search").get(searchSquare);
 
 export default squareRouter;
