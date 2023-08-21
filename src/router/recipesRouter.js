0e42f1a28a2ebdefe870ef63eea12a3940a4bc2b
kangkyeongseo <kks_big@naver.com>
2023-08-21 22:15:28 +0900

    style: recipesRouter.js 주석 추가

--- src/router/recipesRouter.js
+++ src/router/recipesRouter.js
@@ -10,20 +10,25 @@ import {
   getRecipeDelete,
 } from "../controllers/recipesController";
 
+// Recipe Router를 생성합니다.
 const recipesRouter = express.Router();
-
+// Recipe Add Router("/recipe/add")
 recipesRouter
   .route("/add")
   .all(UserOnlyMiddleware)
   .get(getRecipeAdd)
   .post(thumbUpload.single("thumb"), postRecipeAdd);
+// Recipe Search Router("/recipe/search")
 recipesRouter.get("/search", getRecipeSearch);
+// Recipe Detail Router("/recipe/:id")
 recipesRouter.get("/:id", getRecipeDetail);
+// Recipe Edit Router("/recipe/:id/edit")
 recipesRouter
   .route("/:id/edit")
   .all(UserOnlyMiddleware)
   .get(getRecipeEdit)
   .post(thumbUpload.single("thumb"), postRecipeEdit);
+// Recipe Delete Router("/recipe/:id/delete")
 recipesRouter.route("/:id/delete").all(UserOnlyMiddleware).get(getRecipeDelete);
 
 export default recipesRouter;
