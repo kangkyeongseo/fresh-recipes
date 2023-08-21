cb1c2e5b5a7b06d252f3aa723a5691d2cb77d7d8
kangkyeongseo <kks_big@naver.com>
2023-08-21 16:17:07 +0900

    style: ingredientsRouter.js 주석 추가

--- src/router/IngredientsRouter.js
+++ src/router/IngredientsRouter.js
@@ -10,20 +10,25 @@ import {
   postIngEdit,
 } from "../controllers/ingredientsController";
 
+// Ingredient Router를 생성합니다.
 const ingredientsRouter = express.Router();
-
+// Ingredient Add Router("/ingredient/add")
 ingredientsRouter
   .route("/add")
   .all(UserOnlyMiddleware)
   .get(getIngAdd)
   .post(postIngAdd);
+// Ingredient Search Router("/ingredient/search")
 ingredientsRouter.get("/search", getIngSearch);
+// Ingredient Detail Router("/ingredient/:id")
 ingredientsRouter.route("/:id").get(getIngDetail);
+// Ingredient Edit Router("/ingredient/:id/:edit")
 ingredientsRouter
   .route("/:id/edit")
   .all(UserOnlyMiddleware)
   .get(getIngEdit)
   .post(postIngEdit);
+// Ingredient Delete Router("/ingredient/delete")
 ingredientsRouter
   .route("/:id/delete")
   .all(UserOnlyMiddleware)
