a0e96b9e67229b5d02a99fb1b295ab44f7721585
kangkyeongseo <kks_big@naver.com>
2023-08-22 15:20:31 +0900

    style: Recipe.js 주석 추가

--- src/model/Recipe.js
+++ src/model/Recipe.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-
+// Recipe 데이터의 Schema를 생성합니다.
 const recipeSchema = new mongoose.Schema({
   name: { type: String, require: true, trim: true },
   description: { type: String, require: true },
@@ -19,11 +19,12 @@ const recipeSchema = new mongoose.Schema({
       content: { type: String, require: true },
     },
   ],
+  // populate를 사용하기 위한 type과 ref입니다.
   comments: [{ type: mongoose.Schema.Types.ObjectId, ref: "Comment" }],
   owner: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
   likes: [{ type: String }],
 });
-
+// recipeSchema를 이용하여 Recipe Model을 생성합니다.
 const Recipe = mongoose.model("Recipe", recipeSchema);
 
 export default Recipe;
