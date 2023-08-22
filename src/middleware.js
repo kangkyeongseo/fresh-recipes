674213c8deb033c63038e51e781bb73b9957b4e4
kangkyeongseo <kks_big@naver.com>
2023-08-22 15:21:00 +0900

    style: middleware.js 주석 추가

--- src/middleware.js
+++ src/middleware.js
@@ -1,12 +1,12 @@
 import multer from "multer";
-
+// Pug에서 사용하기 위해 res.locals에 session 정보를 저장합니다.
 export const sessionMiddleware = (req, res, next) => {
   res.locals.loggedIn = Boolean(req.session.loggedIn);
   res.locals.loggedInUser = req.session.user || {};
   res.locals.homeTitle = "Fresh Recipes";
   return next();
 };
-
+// req.session.loggedIn false일 떄 redirect 시킵니다.
 export const UserOnlyMiddleware = (req, res, next) => {
   if (!req.session.loggedIn) {
     req.flash("error", "로그인 부탁 드립니다.");
@@ -14,7 +14,7 @@ export const UserOnlyMiddleware = (req, res, next) => {
   }
   next();
 };
-
+// req.session.loggedIn true 떄 redirect 시킵니다.
 export const PublicOnlyMiddleware = (req, res, next) => {
   if (req.session.loggedIn) {
     req.flash("error", "허용되지 않는 경로입니다.");
@@ -22,14 +22,14 @@ export const PublicOnlyMiddleware = (req, res, next) => {
   }
   next();
 };
-
+// avatar 이미지를 업로드하기 위한 multer입니다.
 export const avatarUpload = multer({
   dest: "uploads/avatars",
   limits: {
     fieldSize: 3000000,
   },
 });
-
+// thumbnail 이미지를 업로드하기 위한 multer입니다.
 export const thumbUpload = multer({
   dest: "uploads/thumbs",
   limits: {
