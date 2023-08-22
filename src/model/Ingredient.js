5f4ddc93abb218bef647ef04ea129209f0301066
kangkyeongseo <kks_big@naver.com>
2023-08-22 15:20:41 +0900

    style: Ingredient.js 주석 추가

--- src/model/Ingredient.js
+++ src/model/Ingredient.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-
+// Ingredient 데이터의 Schema를 생성합니다.
 const ingSchema = new mongoose.Schema({
   name: { type: String, require: true, trim: true },
   type: { type: String, require: true },
@@ -9,9 +9,10 @@ const ingSchema = new mongoose.Schema({
   purchaseDate: { type: String, require: true },
   periodLife: { type: String, require: true },
   purchase: { type: Boolean, default: false },
+  // populate를 사용하기 위한 type과 ref입니다.
   owner: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
 });
-
+// ingSchema 이용하여 Ingredient Model을 생성합니다.
 const Ingredient = mongoose.model("Ingredient", ingSchema);
 
 export default Ingredient;
