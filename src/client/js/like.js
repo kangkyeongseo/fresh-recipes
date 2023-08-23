eb5f4d19414fee11ef4276adfa748c4828916935
kangkyeongseo <kks_big@naver.com>
2023-08-23 16:05:09 +0900

    style: like.js 주석 추가

--- src/client/js/like.js
+++ src/client/js/like.js
@@ -4,20 +4,22 @@ const fillHeart = document.querySelector(".fas.fa-heart");
 const likesCount = document.querySelector(".likes__count");
 
 const { id } = recipe.dataset;
-
+// emptyHeart를 click할 시 실행합니다.
 const handleEmptyHeart = async () => {
   fillHeart.classList.add("hidden");
   emptyHeart.classList.remove("hidden");
+  // Recipe와 User 데이터의 likes 속성을 삭제할 api를 호출합니다.
   await fetch(`/api/recipe/${id}/like/delete`, {
     method: "POST",
   });
   const currentCount = likesCount.innerText;
   likesCount.innerText = +currentCount - 1;
 };
-
+// fillHeart click할 시 실행합니다.
 const handleFillHeart = async () => {
   emptyHeart.classList.add("hidden");
   fillHeart.classList.remove("hidden");
+  // Recipe와 User 데이터의 likes 속성을 추가할 api를 호출합니다.
   await fetch(`/api/recipe/${id}/like/add`, {
     method: "POST",
   });
