1c3dfcfb59e2203739a5dbb24bf60ca1359a953c
kangkyeongseo <kks_big@naver.com>
2023-08-23 16:04:54 +0900

    style: user.js 주석 추가

--- src/client/js/user.js
+++ src/client/js/user.js
@@ -4,9 +4,10 @@ const avatarIcon = avatarContainer.querySelector(
   ".user__edit__avatar__no--image"
 );
 const fileInput = document.querySelector(".user__file");
-
+// fileInput이 change할 시 실행합니다.
 const handleFileInput = (event) => {
   const file = event.target.files[0];
+  // createObjectURL 메서드를 사용하여 file을 DOMString으로 반환합니다.
   const fileUrl = URL.createObjectURL(file);
   if (avatarImage) {
     avatarImage.src = fileUrl;
